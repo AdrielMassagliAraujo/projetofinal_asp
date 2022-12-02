3e20304169bb537944967453ad897dab56961a77
AdrielMassagliAraujo <adrielmassagli@gmail.com>
2022-12-02 19:17:59 -0300

    Add project files.

---
+++ projeto2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using projeto2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projeto2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Persona()
+        {
+            var persona = new persona(); //criando o objeto para a classe
+            return View(persona);        //retornar para a view os dados da classe
+        }
+
+        [HttpPost]
+        public ActionResult Persona(persona persona)
+        {
+            if (ModelState.IsValid) //validando o estado, ou seja, verificando a requisição
+            {
+                return View("Resultado", persona); //retorna para a view Resultado
+            }
+            return View(persona);
+        }
+
+        public ActionResult Resultado(persona usuario) //criando a ação para a view resultado
+        {
+            return View(usuario);
+        }
+    }
+}
---
+++ projeto2/Models/persona.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projeto2.Models
+{
+    public class persona
+    {
+
+        [Display(Name = "Nome do Personagem:")]
+        [Required(ErrorMessage = "obrigatório")]
+        public String NomePersona { get; set; }
+
+        [Display(Name = "Idade do Personagem:")]
+        [Required(ErrorMessage = "obrigatório")]
+        public String Idade { get; set; }
+
+        [Display(Name = "Personalidade:")]
+        [Required(ErrorMessage = "obrigatório")]
+        [StringLength(80, MinimumLength = 15, ErrorMessage = "Insira de 15 a 80 caracteres")]
+        public String Personalidade { get; set; }
+
+        [Display(Name = "Frase mais dita:")]
+        [Required(ErrorMessage = "obrigatório")]
+        [StringLength(80, MinimumLength = 15, ErrorMessage = "Insira de 15 a 80 caracteres")]
+        public String Frase { get; set; }
+
+        [Display(Name = "Nome do Ator:")]
+        [Required(ErrorMessage = "obrigatório")]
+        public String NomeAtor { get; set; }
+
+        [Display(Name = "Idade do Ator:")]
+        [Required(ErrorMessage = "obrigatório")]
+        public String IdadeAtor { get; set; }
+
+
+
+
+    }
+}
